cc8f9a9d3e3a2aa16ee0a736deb583c2d766126f
Anastasia Stepanchenko <stepanchenko.anastasia@gmail.com>
2017-12-14 23:35:53 +0100

    better version

--- MXNet/FashionMNIST_MXNet_Demo_Python3.py
+++ MXNet/FashionMNIST_MXNet_Demo_Python3.py
@@ -24,7 +24,9 @@ import mxnet as mx
 import mxnet.notebook.callback
 import numpy as np
 import os
-#import urllib  # for Python 2
+import operator
+import logging
+#import urllib         # for Python 2
 import urllib.request  # for Python 3
 import gzip
 import struct
@@ -33,12 +35,13 @@ import cv2
 import scipy.misc
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import skimage.transform
 
 
 # ### Prepare training and test datasets
 # `download_data` downloads data from a given `url`, if no file with the same name has been downloaded so far,  
 # `to4d` reshapes data, adding one axis of length 1 and normalizes the entries to [0,1],  
-# `read_data` creates labels and datasets from the corresponding .gz files.
+# `read_Fashion_MNIST_data_compact` creates labels and datasets from the corresponding .gz files.
 # 
 # With these functions we download data from GitHub, create training/test datasets/labels and then split them into batches of size 100 (shuffle for training).  
 # Dataset gets the name `'fashion_data'`, labels - `'fashion_item_label'`
@@ -48,35 +51,55 @@ import matplotlib.pyplot as plt
 
 def download_data(url, force_download=True): 
     fname = url.split("/")[-1]
+    
     if force_download or not os.path.exists(fname):
         urllib.request.urlretrieve(url, fname)
+        
     return fname
 
-def to4d(img):
-    return img.reshape(img.shape[0], 1, 28, 28).astype(np.float32)/255
 
-def read_data(label, image):
+def to4d(img):
+    return img.reshape(img.shape[0], 1, 28, 28).astype(np.float32) / 255
+
+
+def read_Fashion_MNIST_data_compact(label, image):
     base_url = 'https://github.com/zalandoresearch/fashion-mnist/raw/master/data/fashion/'
-    with gzip.open(download_data(base_url+label, os.path.join('data',label))) as flbl:
-        magic, num = struct.unpack(">II", flbl.read(8))
+    
+    with gzip.open(download_data(base_url + label, False)) as flbl:
+        flbl.read(8)
         label = np.fromstring(flbl.read(), dtype=np.int8)
-    with gzip.open(download_data(base_url+image, os.path.join('data',image)), 'rb') as fimg:
+        
+    with gzip.open(download_data(base_url + image, False), 'rb') as fimg:
+        # will look up "struct.unpack" later
         magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
         image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
+        
     return (label, image)
 
+
 batch_size = 100
-(train_lbl, train_img) = read_data('train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz')
-(val_lbl, val_img) = read_data('t10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz')
+
+(train_lbl, train_img) = read_Fashion_MNIST_data_compact('train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz')
+(val_lbl, val_img)     = read_Fashion_MNIST_data_compact('t10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz')
+
 train_data_iter = mx.io.NDArrayIter(data={'fashion_data': to4d(train_img)}, label= {'fashion_item_label': train_lbl}, batch_size=batch_size, shuffle=True)
 test_data_iter = mx.io.NDArrayIter(data={'fashion_data': to4d(val_img)}, label= {'fashion_item_label': val_lbl}, batch_size=batch_size)
+
+
+# Each training and test example is assigned to the labels from 0 to 9, here is what the numeric labels correspond to:
+
+# In[3]:
+
+
+fashion_labels=['T-shirt/top','Trouser','Pullover','Dress','Coat',
+                'Sandal','Shirt','Sneaker','Bag','Ankle boot']
 
 
 # ### Display example training data
 # 
 # The original Fashion-MNIST consists of 28x28 grayscale images.
 
-# In[3]:
+# In[4]:
 
 
 for i in range(10):
@@ -86,24 +109,9 @@ for i in range(10):
     plt.axis('off')
 
 plt.show()
-print('label: %s' % (train_lbl[0:10],))
 
-
-# Each training and test example is assigned to the labels from 0 to 9, here is what the numeric labels correspond to:
-
-# In[4]:
-
-
-fashion_labels=['T-shirt/top',
-'Trouser',
-'Pullover',
-'Dress',
-'Coat',
-'Sandal',
-'Shirt',
-'Sneaker',
-'Bag',
-'Ankle boot']
+print('labels: %s' % (train_lbl[0:10],))
+print(operator.itemgetter(*train_lbl[0:10])(fashion_labels))
 
 
 # ### Build MXNet model
@@ -116,8 +124,10 @@ fashion_labels=['T-shirt/top',
 
 
 fashion_item_label = mx.sym.Variable('fashion_item_label')
+
 # input
 data = mx.symbol.Variable('fashion_data')
+
 # Flatten the data from 4-D shape into 2-D (batch_size, num_channel*width*height)
 data = mx.sym.flatten(data=data, name='flatten')
 
@@ -134,26 +144,24 @@ fc1  = mx.sym.FullyConnected(data=data, num_hidden=128)
 act1 = mx.sym.Activation(data=fc1, act_type="relu")
 
 # 2nd fully-connected layer + activation function
-fc2  = mx.sym.FullyConnected(data=act1, num_hidden = 64)
+fc2  = mx.sym.FullyConnected(data=act1, num_hidden=64)
 act2 = mx.sym.Activation(data=fc2, act_type="relu")
 
 # 3rd fully connected layer (MNIST uses 10 classes)
 fc3  = mx.sym.FullyConnected(data=act2, num_hidden=10)
 
 # softmax with cross entropy loss
-mlp  = mx.sym.SoftmaxOutput(data = fc3, label = fashion_item_label, name='softmax')
+mlp  = mx.sym.SoftmaxOutput(data=fc3, label=fashion_item_label, name='softmax')
 
 mx.viz.plot_network(mlp)
 
 
 # ### Train the model and commit checkpoints
 # 
-# First, set up logging and the context (now CPU is used).
+# First, set up logging to INFO level and the context (now CPU is used).
 
 # In[7]:
 
-
-import logging
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger().setLevel(logging.INFO)
@@ -197,12 +205,12 @@ mod = mx.mod.Module(symbol=mlp, data_names=['fashion_data'], label_names=['fashi
 #mod.bind(data_shapes=train_data_iter.provide_data, label_shapes=train_data_iter.provide_label)
 #mod.init_params(initializer=mx.init.Xavier(magnitude=2.))
                         
-mod.fit(train_data_iter,                            # train data
-        eval_data=test_data_iter,                   # validation data
-        optimizer='sgd',                            # use SGD to train
-        optimizer_params={'learning_rate' : 0.1},   # use fixed learning rate
-        eval_metric=mx.metric.Accuracy(),           # report accuracy during training
-        num_epoch=10,                               # train for at most 10 dataset passes
+mod.fit(train_data_iter,                              # train data
+        eval_data = test_data_iter,                   # validation data
+        optimizer ='sgd',                             # use SGD to train
+        optimizer_params = {'learning_rate' : 0.1},   # use fixed learning rate
+        eval_metric = mx.metric.Accuracy(),           # report accuracy during training
+        num_epoch = 10,                               # train for at most 10 dataset passes
         epoch_end_callback = mx.callback.do_checkpoint('fashion_mnist'), 
         force_rebind = True,
         force_init = True) 
@@ -217,17 +225,23 @@ mod.fit(train_data_iter,                            # train data
 
 
 pred_data_iter = mx.io.NDArrayIter(data={'fashion_data': to4d(val_img)[0:100]}, batch_size=100)
-pred_digits = mod.predict(eval_data=pred_data_iter).asnumpy()
+pred_digits    = mod.predict(eval_data=pred_data_iter).asnumpy()
 
 get_ipython().run_line_magic('matplotlib', 'inline')
-import matplotlib.pyplot as plt
-for i in range(10):
-    plt.subplot(1,10,i+1)
-    plt.imshow(val_img[i + 10], cmap='Greys')
+
+# starting index of items we want to predict
+pred_start  = 10
+# number of items we want to predict
+pred_number = 10
+
+for i in range(pred_number):
+    plt.subplot(1,pred_number,i+1)
+    plt.imshow(val_img[i + pred_start], cmap='Greys')
     plt.axis('off')
+    
 plt.show()
 
-for x in range(10, 20):
+for x in range(pred_start, pred_start + pred_number):
     label = np.where(pred_digits[x,0:10] == pred_digits[x,0:10].max())[0]
     print("Predicted fashion label for image %s is %s (%s)" % (x, label, fashion_labels[label[0]]))
 
@@ -250,7 +264,8 @@ urllib.request.urlretrieve('https://images-eu.ssl-images-amazon.com/images/I/41h
 
 
 prediction_model_check_point = 10
-prediction_model_prefix = 'fashion_mnist'
+prediction_model_prefix      = 'fashion_mnist'
+
 prediction_sym, arg_params, aux_params = mx.model.load_checkpoint(prediction_model_prefix, prediction_model_check_point)
 prediction_model = mx.mod.Module(symbol=prediction_sym, data_names=['fashion_data'], label_names=['fashion_item_label'])
 
@@ -288,8 +303,6 @@ def predict_fashion(img):
 
 # In[15]:
 
-
-import skimage.transform
 
 for i in range(3):
         img = mpimg.imread('predict'+str(i+1)+'.jpg')
